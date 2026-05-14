e58c003f539cbe0acb3613e6db78356e581d9138
sunyoung <judy76c@gmail.com>
2026-05-14 15:34:40 +0900

    Delete 새 폴더 directory

--- 새 폴더/js/modules/fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('injection.js', 'utf8');
-c = c.replace(/\$ \s*\{/g, '${');
-c = c.replace(/\?\s+\./g, '?.');
-fs.writeFileSync('injection.js', c);
